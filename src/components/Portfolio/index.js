1ebcb8cf03ccc5f96b90685c89be33a365b24be7
nitrotap <9121718+nitrotap@users.noreply.github.com>
2022-06-23 20:50:13 -0600

    adding extra content to pw generator

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -21,8 +21,7 @@ function Portfolio() {
             alt_text: 'mental health check screen shot',
             gitHubLink: 'https://github.com/nitrotap/mental-health-check',
             deployedLink: 'https://nitrotap.github.io/mental-health-check/',
-            description: `Mental Health Check is a quiz app that helps users identify their feelings and directs them to helpful resources.
-            `
+            description: `Mental Health Check is a quiz app that helps users identify their feelings and directs them to helpful resources. The app uses the MERN stack with Apollo/GraphQL combined with Chart.js, Material UI, Heroku, bcrypt, and JSON Web Tokens.`
         },
         {
             name: 'Traveler\'s Hidden Gems',
@@ -30,7 +29,15 @@ function Portfolio() {
             alt_text: 'traveler\'s hidden gems screen shot',
             gitHubLink: 'https://github.com/nitrotap/travelers-hidden-gems',
             deployedLink: 'https://sleepy-woodland-16634.herokuapp.com/',
-            description: `A travel guide to hidden spots across the United States. Find the offbeat, see something interesting, and share it with others!`
+            description: `A travel guide to hidden spots across the United States. Find the offbeat, see something interesting, and share it with others! This app uses leaflet.js, GeoApify API, bcrypt, express-cookies, MySQL and Sequelize ORM, Bootstrap, Handlebars.js, and OpenStreetMaps`
+        },
+        {
+            name: 'Silver Train',
+            screenshot: stScreenShot,
+            alt_text: '',
+            gitHubLink: 'https://github.com/nitrotap/silver-train',
+            deployedLink: 'https://desolate-retreat-11992.herokuapp.com/',
+            description: `A Progressive Web Application (PWA) for tracking your budget. Offline transactions are uploaded to the database when the connection to the internet is re-established.`
         },
         {
             name: 'Password Generator',
@@ -38,7 +45,7 @@ function Portfolio() {
             alt_text: 'password generator screen shot',
             gitHubLink: 'https://github.com/nitrotap/generate-pw',
             deployedLink: 'https://nitrotap.github.io/generate-pw/',
-            description: `A simple password generator using inquirer and node.js.`
+            description: `A simple password generator using inquirer and node.js. Upon clicking 'Generate Password', the user is guided through simple alerts to decide the strength of their password.`
         },
         {
             name: 'Code Quiz',
@@ -48,14 +55,6 @@ function Portfolio() {
             deployedLink: 'https://nitrotap.github.io/code-quiz/',
             description: `A coding game styled as a quiz to test your coding knowledge.
             `
-        },
-        {
-            name: 'Silver Train',
-            screenshot: stScreenShot,
-            alt_text: '',
-            gitHubLink: 'https://github.com/nitrotap/silver-train',
-            deployedLink: 'https://desolate-retreat-11992.herokuapp.com/',
-            description: `A Progressive Web Application (PWA) for tracking your budget. This app is installable on your phone or computer and can track your budget online and offline. Offline transactions are uploaded to the database when the connection to the internet is re-established.`
         },
         {
             name: 'Line Mash',
@@ -75,7 +74,7 @@ function Portfolio() {
                     Portfolio
                 </h3>
 
-                {<div className="row">
+                {<div className="row card-container">
 
                     {projectList.map((project) => {
                         return (
