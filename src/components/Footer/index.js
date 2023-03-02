ebf614de1a73a0c70271f1bc3891c9fd0b6c3348
nitrotap <9121718+nitrotap@users.noreply.github.com>
2023-03-01 23:02:06 -0700

    fixing link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className='footer'>
                 <h4 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>Made with <span style={{ color: 'peachpuff' }}>&nbsp;&#x2764;&nbsp;</span> in CO.</h4>
                 <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
-                    <a id="profile-link" href="https://github.com/freecodecamp" rel="noreferrer" target="_blank" className="btn contact-details"><i className="fab fa-github"></i> GitHub</a>
+                    <a id="profile-link" href="https://github.com/nitrotap" rel="noreferrer" target="_blank" className="btn contact-details"><i className="fab fa-github"></i> GitHub</a>
                     <a href="https://www.linkedin.com/in/kjevaji/" target="_blank" rel="noreferrer" className="btn contact-details"><i className="fab fa-linkedin"></i> LinkedIn</a>
                     <a href="mailto:kartikinpublic@gmail.com" className="btn contact-details"><i className="fas fa-at"></i> Email</a>
                 </div>
